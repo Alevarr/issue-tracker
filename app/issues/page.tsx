86a6337dfab870f7b98739294c6ee9bbc5916ba9
Alevar <alevar03@mail.ru>
2023-11-14 15:33:22 +0300

    Build fix

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -23,7 +23,7 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row>
+            <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
