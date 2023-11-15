bcd697ae299fb2d3f36d2a672f19d9d133b0985b
Alevar <alevar03@mail.ru>
2023-11-16 02:01:48 +0300

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <Pagination currentPage={10} itemCount={100} pageSize={10} />;
+export default function Home({
+  searchParams: { page },
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <Pagination
+      currentPage={parseInt(page) || 1}
+      itemCount={100}
+      pageSize={10}
+    />
+  );
 }
